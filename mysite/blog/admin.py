df4f8f41a9335b6f77dd5cbb4b00b3ca146d7ffb
DonatasNoreika <donoras@gmail.com>
2025-08-18 13:50:00 +0300

    3 dalis done

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -1,5 +1,19 @@
 from django.contrib import admin
 from .models import Post, Comment
 
-admin.site.register(Post)
-admin.site.register(Comment)
+class CommentInLine(admin.TabularInline):
+    model = Comment
+    extra = 0
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['post', 'author', 'date_created']
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'title', 'date_created', 'author', 'comments_count']
+    list_filter = ['author']
+    list_editable = ['title', 'author']
+    search_fields = ['title', 'content']
+    inlines = [CommentInLine]
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Comment, CommentAdmin)
--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,17 +1,23 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
 class Post(models.Model):
+    author = models.ForeignKey(to=User, on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
     content = models.TextField()
     date_created = models.DateTimeField(auto_now_add=True)
+
+    def comments_count(self):
+        return self.comments.all().count()
 
     def __str__(self):
         return self.title
 
 
 class Comment(models.Model):
-    post = models.ForeignKey(to="Post", on_delete=models.CASCADE)
+    author = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    post = models.ForeignKey(to="Post", on_delete=models.CASCADE, related_name="comments")
     content = models.TextField()
     date_created = models.DateTimeField(auto_now_add=True)
 
