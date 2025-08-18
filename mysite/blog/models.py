0dde711320f21c342d23e3a8644a3166c5f3d149
DonatasNoreika <donoras@gmail.com>
2025-08-18 13:37:44 +0300

    2 dalis done

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(to="Post", on_delete=models.CASCADE)
+    content = models.TextField()
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.post} ({self.date_created})"
