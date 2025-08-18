89272d3efac2e55dfce1c8845f6b4b1cdf489cda
DonatasNoreika <donoras@gmail.com>
2025-08-18 14:03:19 +0300

    4 dalis done

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
+from .models import Post, Comment
+from django.contrib.auth.models import User
 
 def index(request):
-    return render(request, template_name="index.html")
+    context = {
+        'num_posts': Post.objects.count(),
+        'num_comments': Comment.objects.count(),
+        'num_users': User.objects.count(),
+    }
+    return render(request, template_name="index.html", context=context)
 
